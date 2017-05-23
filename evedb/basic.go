9ceea455a3b210a694be0cef3fef13510b11130d
James Horton <valeness84@gmail.com>
2017-05-23 02:33:59 +0000

    Initial working market commit

--- evedb/basic.go
+++ evedb/basic.go
@@ -11,6 +11,18 @@ type Race struct {
 	ShortDescription string
 }
 
+type Blueprint struct {
+	ID int
+	Materials []Material
+}
+
+type Material struct {
+	ID int
+	Name string
+	MaterialID int
+	Quantity int
+}
+
 func (r Race) Icon() string {
 	// TODO: This is coupled to the web server, move it somewhere else
 	if r.IconFile == "" {
@@ -52,4 +64,40 @@ func (e *EveDB) GetRaces() ([]*Race, error) {
 		return nil, err
 	}
 	return res, nil
+}
+
+// GetRaces fetches all Races from the database.
+func (e *EveDB) GetBlueprint(/* typeID int */) (*Blueprint, error) {
+	c, err := e.pool.Open()
+	if err != nil {
+		return nil, err
+	}
+	defer c.Close()
+	rs, err := c.Query(
+		`SELECT
+			  mats."typeID" as typeID,
+			  type."typeName" as typeName,
+			  type."typeID" as materialID,
+			  mats."quantity"
+			FROM evesde."invTypeMaterials" mats
+			INNER JOIN evesde."invTypes" type ON type."typeID" = mats."materialTypeID"
+			WHERE mats."typeID" IN(16242) `) // TODO: Pass in the typeID
+
+	if err != nil {
+		return nil, err
+	}
+	defer rs.Close()
+	res := []Material{}
+	for rs.Next() {
+		r := Material{}
+		err := rs.Scan(&r.ID, &r.Name, &r.MaterialID, &r.Quantity)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, r)
+	}
+	if err = rs.Err(); err != nil {
+		return nil, err
+	}
+	return &Blueprint{Materials: res}, nil
 }

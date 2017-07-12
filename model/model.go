6c8659338e09d05269ae4b3a0ad35b066ce5ea5f
Tyler Sommer <sommertm@gmail.com>
2017-07-11 20:50:16 -0600

    Clean up, add commentary

--- model/model.go
+++ model/model.go
@@ -1,3 +1,4 @@
+// Package model encapsulates the persistence layer.
 package model
 
 import (
@@ -7,6 +8,10 @@ import (
 	"github.com/motki/motkid/evedb"
 )
 
+// A Manager is used to retrieve and save data.
+//
+// Most data is stored in the configured database and only
+// fetched from external sites when necessary.
 type Manager struct {
 	pool   *db.ConnPool
 	evedb  *evedb.EveDB
@@ -14,6 +19,7 @@ type Manager struct {
 	ec     *evecentral.EveCentral
 }
 
+// NewManager creates a new Manager, ready for use.
 func NewManager(pool *db.ConnPool, evedb *evedb.EveDB, api *eveapi.EveAPI, ec *evecentral.EveCentral) *Manager {
 	return &Manager{pool: pool, evedb: evedb, eveapi: api, ec: ec}
 }

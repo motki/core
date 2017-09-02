626cfa7bbdeba7f13ebface4c08d65806fe99a60
Tyler Sommer <sommertm@gmail.com>
2017-09-01 21:00:55 -0600

    Rename production lines to production chains

--- model/product.go
+++ model/product.go
@@ -10,6 +10,7 @@ import (
 	"github.com/shopspring/decimal"
 )
 
+// ProductKind describes the way a product is acquired.
 type ProductKind string
 
 const (
@@ -17,6 +18,7 @@ const (
 	ProductManufacture             = "build"
 )
 
+// Product represents one part of a production chain.
 type Product struct {
 	ProductID      int
 	TypeID         int
@@ -30,6 +32,7 @@ type Product struct {
 	corporationID int
 }
 
+// Cost returns the total cost for the completed product.
 func (p Product) Cost() decimal.Decimal {
 	if p.Kind == ProductBuy {
 		return p.MarketPrice
@@ -41,10 +44,11 @@ func (p Product) Cost() decimal.Decimal {
 	return cost
 }
 
+// NewProduct creates a new production chain for the given corporation and type.
 func (m *Manager) NewProduct(corpID int, typeID int) (*Product, error) {
 	bp, err := m.evedb.GetBlueprint(typeID)
 	if err != nil {
-		return nil, errors.Wrapf(err, "unable to create production line for typeID %d", typeID)
+		return nil, errors.Wrapf(err, "unable to create production chain for typeID %d", typeID)
 	}
 	p := &Product{
 		corporationID:  corpID,
@@ -60,17 +64,18 @@ func (m *Manager) NewProduct(corpID int, typeID int) (*Product, error) {
 		part.Kind = ProductBuy
 		part.Quantity = mat.Quantity
 		if err != nil {
-			return nil, errors.Wrapf(err, "unable to create production line for typeID %d", typeID)
+			return nil, errors.Wrapf(err, "unable to create production chain for typeID %d", typeID)
 		}
 		p.Materials = append(p.Materials, part)
 	}
 	return p, nil
 }
 
+// UpdateProductMarketPrices fetches the latest market data for the production chain.
 func (m *Manager) UpdateProductMarketPrices(product *Product, regionID int) error {
 	stat, err := m.GetMarketStatRegion(regionID, product.TypeID)
 	if err != nil {
-		return errors.Wrapf(err, "unable to update production line market price for typeID %d", product.TypeID)
+		return errors.Wrapf(err, "unable to update production chain market price for typeID %d", product.TypeID)
 	}
 	max := decimal.NewFromFloat(1000000000000)
 	var bestSell = max
@@ -92,7 +97,7 @@ func (m *Manager) UpdateProductMarketPrices(product *Product, regionID int) erro
 	product.MarketRegionID = regionID
 	for _, p := range product.Materials {
 		if err = m.UpdateProductMarketPrices(p, regionID); err != nil {
-			return errors.Wrapf(err, "unable to update production line market price for typeID %d", product.TypeID)
+			return errors.Wrapf(err, "unable to update production chain market price for typeID %d", product.TypeID)
 		}
 	}
 	return nil
@@ -104,16 +109,16 @@ func (m *Manager) UpdateProductMarketPrices(product *Product, regionID int) erro
 //
 // If a product is inserted, its ProductID field is updated.
 func (m *Manager) saveProductWithTx(tx *sql.Tx, product *Product) error {
-	var prodID string
-	if prodID = strconv.Itoa(product.ProductID); prodID == "0" {
-		prodID = "DEFAULT"
+	prodID := "DEFAULT"
+	if n := product.ProductID; n > 0 {
+		prodID = strconv.Itoa(n)
 	}
 	var parentID sql.NullInt64
 	if product.parentID > 0 {
 		parentID.Int64 = int64(product.parentID)
 		parentID.Valid = true
 	}
-	r := tx.QueryRow(`INSERT INTO app.production_lines (
+	r := tx.QueryRow(`INSERT INTO app.production_chains (
 		product_id,
 		type_id,
 		market_price,
@@ -123,7 +128,7 @@ func (m *Manager) saveProductWithTx(tx *sql.Tx, product *Product) error {
 		parent_id,
 		corporation_id)
 	VALUES(`+prodID+`, $1, $2, $3, $4, $5, $6, $7)
-	ON CONFLICT ON CONSTRAINT "production_lines_pkey"
+	ON CONFLICT ON CONSTRAINT "production_chains_pkey"
 		 DO UPDATE SET market_price = EXCLUDED.market_price,
 		     market_region_id = EXCLUDED.market_region_id,
 		     kind = EXCLUDED.kind
@@ -136,8 +141,8 @@ func (m *Manager) saveProductWithTx(tx *sql.Tx, product *Product) error {
 		parentID,
 		product.corporationID)
 	id := 0
-	err := r.Scan(&id)
-	if err != nil {
+
+	if err := r.Scan(&id); err != nil {
 		return err
 	}
 	if id == 0 {
@@ -146,13 +151,16 @@ func (m *Manager) saveProductWithTx(tx *sql.Tx, product *Product) error {
 	product.ProductID = id
 	for _, p := range product.Materials {
 		p.parentID = id
-		if err = m.saveProductWithTx(tx, p); err != nil {
+		if err := m.saveProductWithTx(tx, p); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
+// SaveProduct saves the given production chain in the database.
+//
+// This function automatically handles both inserting and updating.
 func (m *Manager) SaveProduct(product *Product) error {
 	c, err := m.pool.Open()
 	if err != nil {
@@ -173,10 +181,12 @@ func (m *Manager) SaveProduct(product *Product) error {
 	return errors.Wrap(tx.Commit(), "couldn't commit db transaction")
 }
 
+// GetAllProducts returns all production chains associated with the given corporation.
 func (m *Manager) GetAllProducts(corpID int) ([]*Product, error) {
 	return m.getProducts(corpID)
 }
 
+// GetProduct returns a production chain for the given corporation and root product.
 func (m *Manager) GetProduct(corpID int, productID int) (*Product, error) {
 	prods, err := m.getProducts(corpID, productID)
 	if err != nil {
@@ -188,26 +198,28 @@ func (m *Manager) GetProduct(corpID int, productID int) (*Product, error) {
 	return prods[0], nil
 }
 
+// getProducts handles fetching production chain components.
 func (m *Manager) getProducts(corpID int, productIDs ...int) ([]*Product, error) {
 	c, err := m.pool.Open()
 	if err != nil {
 		return nil, err
 	}
 	defer c.Close()
-	var ids = []string{}
+	var ids []string
 	for _, id := range productIDs {
 		ids = append(ids, strconv.Itoa(id))
 	}
-	var idClause = ""
+	var idClause string
 	if len(ids) > 0 {
 		idClause = "AND p1.product_id IN(" + strings.Join(ids, ",") + ")"
 	}
+	// This relies on a recursive CTE to generate the list of products needed for the given root products.
 	r, err := c.Query(`WITH RECURSIVE chain(f, t) AS (
-		SELECT NULL::INT, p1.product_id FROM app.production_lines p1 WHERE p1.corporation_id = $1 AND p1.parent_id IS NULL `+idClause+`
+		SELECT NULL::INT, p1.product_id FROM app.production_chains p1 WHERE p1.corporation_id = $1 AND p1.parent_id IS NULL `+idClause+`
 		UNION
 		SELECT c.t, p2.product_id
 			FROM chain c
-			LEFT OUTER JOIN app.production_lines p2 ON p2.parent_id = c.t
+			LEFT OUTER JOIN app.production_chains p2 ON p2.parent_id = c.t
 			WHERE c.t IS NOT NULL
 		)
 		SELECT p.product_id,
@@ -217,7 +229,7 @@ func (m *Manager) getProducts(corpID int, productIDs ...int) ([]*Product, error)
 			 p.quantity,
 			 p.kind,
 			 p.parent_id
-		FROM app.production_lines p
+		FROM app.production_chains p
 		 	JOIN chain c ON c.t = p.product_id
 		WHERE c.t IS NOT NULL ORDER BY p.parent_id NULLS FIRST`, corpID)
 	if err != nil {
